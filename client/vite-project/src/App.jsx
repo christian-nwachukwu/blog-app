55cf3a8e4178ecea16760f2c2ec9634b1751135a
Christian Nwachukwu <christian.nwachukwu2020@gmail.com>
2025-01-04 10:39:31 +0100

    Created the routes linking all pages making use of Outlet, Layout etc

--- client/vite-project/src/App.jsx
+++ client/vite-project/src/App.jsx
@@ -1,15 +1,66 @@
-import { useState } from 'react'
-import './App.css'
+//import './App.css'
+import { createBrowserRouter, RouterProvider, Route, Outlet, } from 'react-router-dom'
+import Register from './pages/Register'
+import Login from './pages/Login'
+import Home from './pages/Home'
+import Post from './pages/Post'
+import Single from './pages/Single'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+const Layout = () => {
   return (
     <>
-      <div>
-        <h3>Blog App</h3>
-      </div>
+      <Navbar />
+      <Outlet />
+      <Footer />
     </>
+  )
+}
+
+// For pages/routes that requires inside the html body, Navbar, Footer, Sections, Hero etc
+const router = createBrowserRouter([
+  {
+  path: "/",
+  element: <Layout/>,
+  children:[
+      {
+        path: "/",
+        element: <Home/>
+      },
+
+      {
+        path: "/post",
+        element: <Post/>
+      },
+
+      {
+        path: "/single",
+        element: <Single/>
+      },
+  ]
+},
+
+// For the pages/routes not requiring the Navbar, Footer etc e.g the Login and Register pages
+{
+  path: "/register",
+  element: <Register/>,
+},
+
+{
+  path: "/login",
+  element: <Login/>,
+},
+])
+
+
+function App() {
+
+  return (
+      <>
+        <RouterProvider router={router}/>
+      </>
   )
 }
 
