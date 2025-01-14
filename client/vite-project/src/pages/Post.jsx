16494f53cfc9376a03e70abbf3c12ddeed979d35
Christian Nwachukwu <christian.nwachukwu2020@gmail.com>
2025-01-14 05:54:45 +0100

    Completion of the front end codes

--- client/vite-project/src/pages/Post.jsx
+++ client/vite-project/src/pages/Post.jsx
@@ -11,7 +11,7 @@ const Post = () => {
       <div className='content'>
         <input type='text' placeholder='Title' />
         <div className='editorContainer'>
-          <ReactQuill theme="snow" value={value} onChange={setValue} />
+          <ReactQuill className='editor' theme="snow" value={value} onChange={setValue} />
         </div>
       </div>
       <div className='menu'>
@@ -23,10 +23,40 @@ const Post = () => {
           <span>
             <b>Visibility: </b> Public
           </span>
-          <input type='file' id='file' name='' />
-          <label htmlFor='file'>Upload Image</label>
+          <input style={{display: 'none'}} type='file' id='file' name='' />
+          <label className='file' htmlFor='file'>Upload Image</label>
+          <div className='buttons'>
+            <button>Save as draft</button>
+            <button>Update</button>
+          </div>
         </div>
-        <div className='item'>l2</div>
+        <div className='item'>
+          <h1>Category</h1>
+          <div className='cat'>
+          <input type='radio' name='cat' value='art' id='art' />
+          <label htmlFor='art'>Art</label>
+          </div>
+          <div className='cat'>
+          <input type='radio' name='cat' value='science' id='science' />
+          <label htmlFor='science'>Science</label>
+          </div>
+          <div className='cat'>
+          <input type='radio' name='cat' value='technology' id='technology' />
+          <label htmlFor='technology'>Technology</label>
+          </div>
+          <div className='cat'>
+          <input type='radio' name='cat' value='cinema' id='cinema' />
+          <label htmlFor='cinema'>Cinema</label>
+          </div>
+          <div className='cat'>
+          <input type='radio' name='cat' value='design' id='design' />
+          <label htmlFor='design'>Design</label>
+          </div>
+          <div className='cat'>
+          <input type='radio' name='cat' value='food' id='food' />
+          <label htmlFor='food'>Food</label>
+          </div>
+        </div>
       </div>
     </div>
   )
